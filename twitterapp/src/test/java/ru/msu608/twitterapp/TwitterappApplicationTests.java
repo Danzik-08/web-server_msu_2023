96bb7dd14669fc3e466968208fbaebff0611d07c
Arsen Ismagilov <ismagilovarsen99@mail.ru>
2023-11-23 16:58:58 +0300

    Added app template from spring quickstart

---
+++ twitterapp/src/test/java/ru/msu608/twitterapp/TwitterappApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.msu608.twitterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
